6072f4faae3040930492f49ad1d12e4b9f3f3a90
covertcorvid <17987483+covertcorvid@users.noreply.github.com>
2024-08-11 20:54:22 -0400

    Adds citadel reaction hooks

--- compile_for_nsv13.sh
+++ compile_for_nsv13.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # TODO: add linux
-cargo t --target=i686-pc-windows-msvc --features "katmos" generate_binds
-cargo rustc --target=i686-pc-windows-msvc --release --features "katmos" -- -C target-cpu=native
+cargo t --target=i686-pc-windows-msvc --features "katmos citadel_reactions" generate_binds
+cargo rustc --target=i686-pc-windows-msvc --release --features "katmos citadel_reactions" -- -C target-cpu=native
+cargo rustc --target i686-unknown-linux-gnu --release --features "katmos citadel_reactions" -- -C target-cpu=native
 echo "Now copy the binary from target/i686-pc-windows-msvc/release/ to the NSV13 repository."
